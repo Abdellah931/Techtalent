6a9c57753ce7dc2d80a60249a45661a6b953d34a
Abdellah931 <158474102+Abdellah931@users.noreply.github.com>
2024-02-06 13:59:54 +0100

    unidad 4 pruebas

---
+++ Ejercicios eclipse/si/src/si/Unidad4preueba2.java
@@ -0,0 +1,15 @@
+package si;
+
+public class Unidad4preueba2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int precioproducto=100;
+	final double IVA=0.21;
+	System.out.printf("informacion del producto"
+			+ "\nEl precio del producto es " + precioproducto + 
+			"\nEl precio del producto incluyendo IVA, es "+ (precioproducto+(precioproducto*IVA)));
+
+	}
+}
---
+++ Ejercicios eclipse/si/src/si/Unidad4prueba1.java
@@ -0,0 +1,13 @@
+package si;
+
+public class Unidad4prueba1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int operador = 10;
+		    operador++;
+		    System.out.println(operador);
+		    System.out.println(operador++);
+		    System.out.println(++operador);		
+		}
+}
