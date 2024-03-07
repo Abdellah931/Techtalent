e934574436a2f09a3bbece500dd1555e57042457
Abdellah931 <158474102+Abdellah931@users.noreply.github.com>
2024-03-07 12:18:47 +0100

    si

--- PruebasUN6/src/pruebas.java
+++ PruebasUN6/src/pruebas.java
@@ -1,4 +1,3 @@
-
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
@@ -6,64 +5,66 @@ import java.util.Random;
 import java.util.Set;
 
 public class pruebas {
+    public static void main(String[] args) {
+        // Nombres y edades
+        String[] nombres = {"Abde", "Laia", "Jess", "Isabel", "Joel", "Alex", "Santos", "Alejandro", "Diego", "Jose", "Sebas", "Ana Maria", "Toni", "Manel", "Aurora"};
+        Integer[] edades = {22, 23, 26, 18, 19, 28, 24, 22, 22, 26, 29, 27, 23, 23, 30};
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int edades[]= {22,23,26,18,19,23,18,22,22,26,26,27,23,23,30};
-		String nombres[]= {"abde", "laia","jess", "isabel","joel","alex","santos","alejandro","diego","jose" ,"sebas" ,"ana maria" ,"toni" ,"manel" ,"aurora"};
-		Person[] personas = new Person[nombres.length];
+        boolean [] asignado= new boolean [nombres.length];
+        
+        // Crear un array de objetos Person con nombres y edades
+        Person[] personas = new Person[nombres.length];
         for (int i = 0; i < nombres.length; i++) {
             personas[i] = new Person(nombres[i], edades[i]);
         }
 
         // Generar e imprimir 5 grupos aleatorios
-        Person[] personas1 = new Person[nombres.length];
-        for (int i = 0; i < nombres.length; i++) {
-            personas1[i] = new Person(nombres[i], edades[i]);
-        }
-
-        // Generar e imprimir 5 grupos aleatorios
         System.out.println("Grupos Aleatorios:");
         for (int i = 0; i < 5; i++) {
-            Person[] grupoAleatorio = generarGrupoAleatorio(personas1);
+            Person[] grupoAleatorio = generarGrupoAleatorio(personas,asignado);
             imprimirGrupo(grupoAleatorio);
         }
 
         // Ordenar las personas por edades de menor a mayor
-        Arrays.sort(personas1, Comparator.comparing(Person::getEdad));
+        Arrays.sort(personas, Comparator.comparing(Person::getEdad));
 
         // Generar e imprimir 5 grupos ordenados por edades
         System.out.println("\nGrupos Ordenados por Edades:");
         for (int i = 0; i < 5; i++) {
-            Person[] grupoOrdenado = Arrays.copyOfRange(personas1, i * 3, (i + 1) * 3);
+            Person[] grupoOrdenado = Arrays.copyOfRange(personas, i * 3, (i + 1) * 3);
             imprimirGrupo(grupoOrdenado);
         }
 
         // Ordenar las personas alfabéticamente
-        Arrays.sort(personas1, Comparator.comparing(Person::getNombre));
+        Arrays.sort(personas, Comparator.comparing(Person::getNombre));
 
         // Generar e imprimir 5 grupos ordenados alfabéticamente
         System.out.println("\nGrupos Ordenados Alfabéticamente:");
         for (int i = 0; i < 5; i++) {
-            Person[] grupoAlfabetico = Arrays.copyOfRange(personas1, i * 3, (i + 1) * 3);
+            Person[] grupoAlfabetico = Arrays.copyOfRange(personas, i * 3, (i + 1) * 3);
             imprimirGrupo(grupoAlfabetico);
         }
     }
 
     // Método para generar un grupo aleatorio
-    private static Person[] generarGrupoAleatorio(Person[] personas) {
+    private static Person[] generarGrupoAleatorio(Person[] personas, boolean [] asignado) {
         Person[] grupo = new Person[3];
         Set<String> nombresUsados = new HashSet<>();
         Random rand = new Random();
         for (int i = 0; i < 3; i++) {
             int index;
             do {
+            	
                 index = rand.nextInt(personas.length);
-            } while (nombresUsados.contains(personas[index].getNombre()));
+                
+                
+            } while (asignado[index]);
+            asignado [index]= true;
             grupo[i] = personas[index];
             nombresUsados.add(personas[index].getNombre());
         }
         return grupo;
+        
     }
 
     // Método para imprimir un grupo de personas
