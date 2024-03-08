725b6fbbcb021b72171813ba0b2a35965aa6afa9
Abdellah931 <158474102+Abdellah931@users.noreply.github.com>
2024-03-08 13:14:01 +0100

    KJ

---
+++ EjerciciosUN7/src/ejercicio2UN7.java
@@ -0,0 +1,94 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import java.util.Iterator;
+
+
+public class ejercicio2UN7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Ingrese la cantidad de productos: ");
+		int cantidadProductos = scanner.nextInt();
+		
+		
+Map<String, Double> PrecioProducto = new HashMap<>();
+		
+		
+		// Indicar las Key(alumnos)
+		for (int i = 1; i <= cantidadProductos; i++) {
+			System.out.print("Ingrese el nombre del producto " + i + ": ");
+			String nombreProducto = scanner.next();
+//			double notaMediaPorAlumno = 0;
+			// Indicar las Value(nota)
+			
+		System.out.print("Ingrese precio producto: ");
+		double precioProductoBruto = scanner.nextDouble();
+		
+		System.out.print("Es producto de primera necesidad: ");
+		String necesidadIVA =  scanner.next();
+		
+		necesidadIVA = necesidadIVA.toLowerCase();
+		double precioProductoNeto;
+		
+		if ( necesidadIVA.equals("si")) {
+			precioProductoNeto = (precioProductoBruto*0.04)+precioProductoBruto;
+            System.out.println("el IVA sera del 4%: "  + precioProductoBruto*0.04 +
+            		"\nEl precio neto es de: " + precioProductoNeto);
+            
+        } else {
+        	precioProductoNeto = (precioProductoBruto*0.21)+precioProductoBruto;
+            System.out.println("El IVA sera del 21%: " + precioProductoBruto*0.21 +
+            		"\nEl precio neto es de: " + precioProductoNeto);
+        }
+            PrecioProducto.put(nombreProducto, precioProductoNeto);
+		}
+            
+		
+		
+		double sumaCompra = 0;
+        	for (double m : PrecioProducto.values()) {
+				sumaCompra += m;
+			}
+
+			double totalCompra = sumaCompra;
+
+				
+				System.out.println( "El precio total de la compra sera de: "+ totalCompra);
+				
+//				System.out.print("El pago del cliente sera ");
+//				double pagoCliente =  scanner.nextDouble();
+//				
+//				double cambioCliente = pagoCliente - totalCompra;
+//				System.out.println("El cliente pago " + pagoCliente + " y el cambio sera de " + cambioCliente);
+//	}
+//			
+			}
+}
+
+	
+		
+		
+		
+		
+		
+//		
+//		double precioTotalBruto = carrito.getPrecioTotalBruto();
+//		double iva = precioTotalBruto * 0.21;
+//		double precioTotalConIVA = precioTotalBruto + iva;
+//
+//		// Creamos al cliente y calculamos su cambio//
+//		Cliente cliente = new Cliente("Juan", 10.0);
+//		double cambio = cliente.getCambio(precioTotalConIVA);
+//
+//		// Finalmente, mostramos la información en la factura//
+//		System.out.println("IVA aplicado (21%): " + iva);
+//		System.out.println("Precio total bruto: " + precioTotalBruto);
+//		System.out.println("Precio total con IVA: " + precioTotalConIVA);
+//		System.out.println("Número de artículos comprados: " + carrito.getNumeroArticulos());
+//		System.out.println("Cantidad pagada: " + cliente.getCantidadPagada());
+//		System.out.println("Cambio a devolver al cliente: " + cambio);
+	
---
+++ EjerciciosUN7/src/ejercicio3UN7.java
@@ -0,0 +1,9 @@
+
+public class ejercicio3UN7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
